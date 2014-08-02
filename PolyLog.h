18a34645ac3faf74ea0d9d171e519d0d2e2d8485
Florian Goth <CaptainSifff@gmx.de>
2014-08-02 23:46:42 +0200

    More implementation details of the PolyLog.

--- PolyLog.h
+++ PolyLog.h
@@ -54,9 +54,9 @@ inline std::complex<FPType> PolyLog_Exp(const FPType s, std::complex<FPType> w)
 	{//FIXME: check for real or non-real argument. asymptotic expansions
 	  while (w.imag() > M_PI) w = std::complex<FPType>(w.real(), w.imag() - 2.0*M_PI);//branch cuts??
 	  while (w.imag() <= -M_PI) w = std::complex<FPType>(w.real(), w.imag() + 2.0*M_PI);
-	  if (fpequal(arg(w), 0.0) || fpequal(arg(w), 2.0*M_PI))
-	    return mytr1::__detail::__riemann_zeta(s);
-	  else
+// 	  if (fpequal(arg(w), 0.0) || fpequal(arg(w), 2.0*M_PI))
+// 	    return mytr1::__detail::__riemann_zeta(s);
+// 	  else
 	    return PolyLog_Exp(static_cast<uint>(nu) , w);
 	}
 	else
@@ -70,9 +70,46 @@ inline std::complex<FPType> PolyLog_Exp(const FPType s, std::complex<FPType> w)
 	  return PolyLog_Exp_neg(s, w);
 	else
 	{
-	  if (fpequal(arg(w), 0.0) || fpequal(arg(w), 2.0*M_PI))
-	    return mytr1::__detail::__riemann_zeta(s);
-	  else
+// 	  if (fpequal(arg(w), 0.0) || fpequal(arg(w), 2.0*M_PI))
+// 	    return mytr1::__detail::__riemann_zeta(s);
+// 	  else
+	    return PolyLog_Exp_pos(s, w);
+	}
+      }
+    }
+    
+template <typename FPType>
+inline std::complex<FPType> PolyLog_Exp(const FPType s, FPType w)
+    {
+      if (fpequal<FPType>(std::rint(s), s))
+      {//capture the cases of positive integer index
+	int nu = static_cast<int> (lrint(s));
+	if(0 == nu)
+	{
+	  FPType t = std::exp(w);
+	  return t/(1.0 - t);
+	}
+	else if (1 == nu)
+	  return -std::log(1.0 - std::exp(w));
+	else if (nu > 1)
+	{//FIXME: check for real or non-real argument. asymptotic expansions
+// 	  if (fpequal(arg(w), 0.0) || fpequal(arg(w), 2.0*M_PI))
+// 	    return mytr1::__detail::__riemann_zeta(s);
+// 	  else
+	    return PolyLog_Exp(static_cast<uint>(nu) , w);
+	}
+	else
+	  return PolyLog_Exp_neg(s, w);
+      }
+      else
+      {//FIXME: check for real or non-real argument. asymptotic expansions
+	if (s < 0)
+	  return PolyLog_Exp_neg(s, w);
+	else
+	{
+// 	  if (fpequal(arg(w), 0.0) || fpequal(arg(w), 2.0*M_PI))
+// 	    return mytr1::__detail::__riemann_zeta(s);
+// 	  else
 	    return PolyLog_Exp_pos(s, w);
 	}
       }
@@ -81,6 +118,28 @@ inline std::complex<FPType> PolyLog_Exp(const FPType s, std::complex<FPType> w)
 template <typename FPType>
 inline std::complex<FPType> PolyLog(const FPType s, std::complex<FPType> w)
 {
+  if(fpequal(arg(w), 0.0))
+    return PolyLog(s, real(w));
+  else
+  {
+    	std::cout<<"domain not (yet) supported!!"<<std::endl;
+	std::__throw_domain_error(__N("Bad argument to PolyLog"));
+  }
+}
+
+template <typename FPType>
+inline std::complex<FPType> PolyLog(const FPType s, FPType w)
+{
+  if (fpequal(w, 0.0)) return 0.0;//According to Mathematica
+  if (w < 0)
+  {//use the square formula to access negative values.
+    return PolyLog(s, w*w)*std::pow(2.0, 1.0-s) - PolyLog(s, -w);
+  }
+  else
+  {
+    FPType x = std::log(w);
+    return PolyLog_Exp(s, w);
+  }
 }
 
 #endif
