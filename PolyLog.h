541051ac80d13e996effa60e7d0d78341f43f7e7
Florian Goth <CaptainSifff@gmx.de>
2014-08-04 00:49:56 +0200

    Add implementation for real negative s.

--- PolyLog.h
+++ PolyLog.h
@@ -75,11 +75,11 @@ inline std::complex<FPType> PolyLog_Exp_pos(const unsigned int s, std::complex<F
  * @param s the index s
  * @param w
  */
-template <typename FPType>
-inline std::complex<FPType> PolyLog_Exp_neg(const unsigned int s, std::complex<FPType> w)
-{//negative integer s
-  //no specialization yet present
-}
+// template <typename FPType>
+// inline std::complex<FPType> PolyLog_Exp_neg(const unsigned int s, std::complex<FPType> w)
+// {//negative integer s
+//   //no specialization yet present
+// }
 
 /** This function catches the cases of negative real index s
  * @param s the index s
@@ -87,7 +87,57 @@ inline std::complex<FPType> PolyLog_Exp_neg(const unsigned int s, std::complex<F
  */
 template <typename FPType>
 inline std::complex<FPType> PolyLog_Exp_neg(const FPType s, std::complex<FPType> w)
-{//negative s
+{//basic general loop, but s is a negative quantity here
+      //TODO: optimize/fix for the case of negative Integers
+      std::cout<<"Negative real s"<<std::endl;
+      std::complex<FPType> res = std::tgamma(1-s)*std::pow(-w, s-1);
+      constexpr FPType tp = 2.0 * M_PI;
+      const std::complex<FPType> wup = w/tp;
+      std::complex<FPType> w2 = wup;
+      std::complex<FPType> pref = std::pow(tp, s)/M_PI;
+      std::complex<FPType> gam = std::tgamma(1.0-s); //here we factor up the ratio of Gamma(1 - s + k)/k! . This ratio should be well behaved even for large k
+      
+      FPType sp, cp;
+      sincos(M_PI/2.0 * s, &sp, &cp);
+      //Here we add the expression that would result from ignoring the zeta function in the series.
+      std::complex<FPType> expis(cp, sp);
+      std::complex<FPType> p = tp - std::complex<FPType>(0.0, 1.0) * w;
+      std::complex<FPType> q = tp + std::complex<FPType>(0.0, 1.0) * w;
+      res += std::complex<FPType>(0.0, 1.0) * gam * (conj(expis) * std::pow(p, s-1.0) - expis *std::pow(q, s-1.0));//this can be optimized for real values
+      //The above expression is the result of sum_k Gamma(1+k-s) /k! * sin(pi /2* (s-k)) * (w/2/pi)^k
+      //Therefore we only need to sample values of zeta(n) on the real axis that really differ from one
+      res += pref * sp * gam * (mytr1::__detail::__riemann_zeta(1-s) - 1.0);
+      const unsigned int maxit = 200;
+      unsigned int j = 1;
+      bool terminate = false;
+      gam*= (1.0 - s);
+      while (!terminate)//assume uniform convergence
+      {
+	FPType rzarg = 1 + j - s;
+	FPType rz = (mytr1::__detail::__riemann_zeta(rzarg) - 1.0);//only the difference to one is needed
+	FPType sine;
+	if(j & 1)//save the repeated recalculation of the sines
+	{/*odd*/
+	  sine = cp;
+	  if ( !((j-1)/ 2 & 1) )
+	    sine = -sine;
+	}
+	else
+	{/*even*/
+	  sine = sp;
+	  if((j/2) & 1)
+	    sine = -sine;
+	}
+	std::complex<FPType> nextterm =  w2 * gam * (sine * rz);
+//	std::cout<<j<<" "<<nextterm<<" "<<rz<<std::endl;
+	w2 *= wup;
+	++j;
+	gam  *= rzarg/(j);//equal to 1/(j+1) since we have incremented j in the line above
+	terminate = (fpequal( std::abs(res + pref*nextterm), std::abs(res) ) || (j > maxit)) && !fpequal(std::abs(rz), 0.0);/*this last check is necessary at integer s*/
+	res += pref*nextterm;
+      }
+      std::cout<<"Iterations in PolyLogExp_neg: "<<j<<std::endl;
+      return res;
 }
 
 /** This function catches the cases of positive real index s
